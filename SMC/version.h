8d35a5da56e62b97a99e49967e7d0767f8bae704
Matthew Oliver <protogonoi@gmail.com>
2014-10-08 00:05:30 +1100

    Add VS project files.

---
+++ SMC/version.h
@@ -0,0 +1,8 @@
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "libilbc 1.1.1"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.1.1"
+
+/* Version number of package */
+#define VERSION "1.1.1"
