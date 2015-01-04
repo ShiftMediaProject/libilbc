7d6cf32c2168484893d440d3a6cf034a9573a111
Matt Oliver <protogonoi@gmail.com>
2015-01-04 20:37:27 +1100

    project: Update for recent merge.

--- SMP/version.h
+++ SMP/version.h
@@ -1,8 +1,8 @@
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libilbc 2.0.0"
+#define PACKAGE_STRING "libilbc 2.0.2"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.0.0"
+#define PACKAGE_VERSION "2.0.2"
 
 /* Version number of package */
-#define VERSION "2.0.0"
+#define VERSION "2.0.2"
